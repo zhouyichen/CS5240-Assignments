e7b60c8851d6998ba8129101f125b833aef34ae2
zhouyichen <zhouyichen123@gmail.com>
2016-08-25 14:32:13 +0800

    [A1] linear fit 2

---
+++ assignment-1/linear-fit-2.py
@@ -0,0 +1,40 @@
+# linear-fit-1.py
+# Example program for linear least square fitting
+
+import numpy as np 
+import numpy.linalg as la
+
+# Read data in input file, which is in CSV format.
+file = open("co2-data.txt")
+data = np.genfromtxt(file, delimiter=",")
+file.close()
+print "data =\n", data
+
+# Arrange data into D matrix and v vector.
+n = len(data[:,0])
+D = np.matrix(np.empty([n,4]))
+D[:,0:3] = data[:,1:4]
+D[:,3] = np.ones([n,1])
+v = np.matrix(data[:,0]).T
+
+# Print to verify that data is arranged correctly.
+print "D =\n", D
+print "v =\n", v
+
+# Solve for least square solution
+a,e,r,s = la.lstsq(D, v)
+print "a =\n", a
+
+# Compute fitting error
+norm = la.norm(D * a - v)
+err = norm * norm
+print "err =", err
+print "lstsq e =", e
+
+# Write fitting result into output file
+result = np.matrix(np.empty([n,4]))
+result[:,0] = D * a
+result[:,1:4] = D[:,0:3]
+file = open("co2-result-l2.txt", "w")
+np.savetxt(file, result, "%f", ",")
+file.close()
